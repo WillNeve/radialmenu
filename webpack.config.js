231265566a145a62ac7d4d32cb18d28ecadf358f
William Neve <williamneve6000@gmail.com>
2023-12-27 04:12:22 +0000

    updated webpack build address to /docs for gh-pages compatability

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     }),
   ],
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./docs"),
     filename: "main.js"
   },
   devtool: "source-map",
